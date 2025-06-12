3880f440208aa57a26b0e1f2d360eb9e905e6f1c
Nikhilkathale741 <95696925+Nikhilkathale741@users.noreply.github.com>
2025-06-12 14:46:51 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
+sed -i 's/\[]/\["51.21.170.1"]/' /home/ubuntu/PCCM/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
